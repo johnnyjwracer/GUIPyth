934123adcb00adab8348ee3d07d5a813fc6a493d
Johnny Wilder <johnnyjwracer@gmail.com>
2023-03-12 00:56:17 -0500

    Added validate entry and button & new image

--- FinalGUI_JW_WIP.py
+++ FinalGUI_JW_WIP.py
@@ -4,11 +4,10 @@ Johnny Wilder 03/06/23
    A python GUI application for the final project that uses tkinter 
    and has multiple buttons, labels, user entry, as well as callback fxns
    and has second window functionality with image display in both windows.
-   Work in Progress
 """
 # Importing tkinter module
 import tkinter as tk
-from tkinter import *
+from tkinter import * #imports all from tkinter to make ease of access calling
 from tkinter import messagebox #import messagebox function from tkinter library
 
 root = tk.Tk() 
@@ -20,26 +19,40 @@ image_label = tk.Label(root, image=image)
 image_label.image = image
 image_label.pack()
 
-labelframe= LabelFrame(root, text= "Hello World", font= ('Century 20 bold'),labelanchor= "n",bd=5,bg= "Purple",width= 1340, height= 400) #Creating a frame for the main window
+labelframe= LabelFrame(root, text= "Hello All!", font= ('Century 20 bold'),labelanchor= "n",bd=5,bg= "Purple",width= 1340, height= 400) #Creating a frame for the main window
 labelframe.pack(expand= True, fill= BOTH)
 
 label = tk.Label(labelframe, text="Welcome to my application!", font= ('Century 12 bold'), fg="Black", bg="Purple" ) # Creating a label and adding it to the main window
 label.pack()
 
+def validate_input(): # Defining a function to validate user input
+    user_input = entry.get() #This function validates the user input in the entry box.
+    
+    if not user_input.isdigit(): # Checking if the user entered a number
+        messagebox.showerror("Error", "Please enter a number!")
+        return
+    
+    if not user_input: # Checking if the user entered a non-empty value
+        messagebox.showerror("Error", "Please enter a value!")
+        return
+
+    user_input = int(user_input)  # Converting the user input to an integer
+    messagebox.showinfo("User Input", f"You entered {user_input}.") # Displaying the user input
+
 def open_second_window(): # Defining a function to open the second window
     second_window = tk.Toplevel(root) # This function opens the second window of the application.
     second_window.title("Second Window")  # Creating the second window
 
-    label = tk.Label(second_window, text="This is the second window!")  # Creating a label and adding it to the second window
+    label = tk.Label(second_window, text="This is the Calculator window!", font= ('Century 12 bold'), fg="Black", bg="Purple" )  # Creating a label and adding it to the second window
     label.pack()
 
-    image = tk.PhotoImage(file="image.png")  # Creating an image and adding it to the second window
+    image = tk.PhotoImage(file="welcome2.png")  # Creating a welcome image and adding it to the second window
     image_label = tk.Label(second_window, image=image)
     image_label.image = image
     image_label.pack()
 
     class Calculator(tk.Frame):
-        def __init__(self, master=None):
+        def __init__(self, master=None): #defining the calculator
             super().__init__(master)
             self.master = master
             self.master.title("Calculator")
@@ -47,12 +60,12 @@ def open_second_window(): # Defining a function to open the second window
             self.pack()
             self.create_widgets()
 
-        def button_click(self, value):
+        def button_click(self, value): #adds button click fxn for the calculator
             current = self.display.get()
             self.display.delete(0, tk.END)
             self.display.insert(0, str(current) + str(value))
 
-        def calculate(self):
+        def calculate(self): #calculating insertion for the calculator
             try:
                 result = eval(self.display.get())
                 self.display.delete(0, tk.END)
@@ -118,6 +131,17 @@ def open_second_window(): # Defining a function to open the second window
     exit_button = tk.Button(second_window, text="Exit", command=close_second_window) # Creating an exit button and adding it to the second window
     exit_button.pack()
 
+label = tk.Label(labelframe, text="Please enter a number value to validate input:", font= ('Century 12 bold'), fg="Black", bg="Purple" )
+label.pack(padx=5, pady=5)
+entry = tk.Entry(labelframe) # Creating an entry box and adding it to the main window
+entry.pack()
+
+validate_button = tk.Button(labelframe, text="Validate", command=validate_input) # Creating a button to validate user input
+validate_button.pack(padx=5, pady=5)
+
+label = tk.Label(labelframe, text="Click button below to go to Calculator", font= ('Century 12 bold'), fg="Black", bg="Purple" )# adds label for calculator window direction
+label.pack(padx=5, pady=5)
+
 open_second_window_button = tk.Button(labelframe, text="Go to Calculator", command=open_second_window) # Creating a button to open the second window
 open_second_window_button.pack(padx=10, pady=5)
 
@@ -126,4 +150,4 @@ def exit_application(): # Defining a function to exit the application
 
 exit # Creating an exit button and adding it to the main window
 
-root.mainloop()
+root.mainloop() #running the program
